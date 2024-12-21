b48cf187d62b02c54f08f85a1897895616864158
abdussametalisar <abdussamet032@gmail.com>
2024-12-21 23:54:06 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,62 +1,69 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+    devtools: {enabled: true},
 
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/supabase',
-    '@nuxt/image',
-    'nuxt-icon',
-  ],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/supabase',
+        '@nuxt/image',
+        '@nuxt/icon'
+    ],
 
-  plugins: [
-    { src: '~/plugins/maska.client.ts', mode: 'client' }
-  ],
+    plugins: [
+        {src: '~/plugins/maska.client.ts', mode: 'client'}
+    ],
 
-  css: ['~/assets/css/main.css'],
+    css: ['~/assets/css/main.css'],
 
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-    }
-  },
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        }
+    },
 
-  image: {
-    provider: 'ipx',
-    quality: 80,
-    format: ['webp'],
-    screens: {
-      xs: 320,
-      sm: 640,
-      md: 768,
-      lg: 1024,
-      xl: 1280,
-      xxl: 1536,
-    }
-  },
+    image: {
+        provider: 'ipx',
+        quality: 80,
+        format: ['webp'],
+        screens: {
+            xs: 320,
+            sm: 640,
+            md: 768,
+            lg: 1024,
+            xl: 1280,
+            xxl: 1536,
+        }
+    },
 
-  nitro: {
-    compressPublicAssets: true,
-     prerender: {
-      routes: [] // Hiçbir route prerender edilmez
-     },
-     preset: 'cloudflare'
-  },
-  
+    nitro: {
+        compressPublicAssets: true,
+        prerender: {
+            routes: [] // Hiçbir route prerender edilmez
+        },
+        preset: 'cloudflare'
+    },
 
-  routeRules: {
-    // SPA geçişleri için
-    '/': { prerender: true },
-    '/products/**': { swr: 3600 },
-    '/categories/**': { swr: 3600 },
-  },
+    runtimeConfig: {
+        public: {
+            SUPABASE_URL: process.env.SUPABASE_URL,
+            SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+        },
+    },
 
-  tailwindcss: {
-    cssPath: '~/assets/css/main.css',
-    configPath: 'tailwind.config.js',
-    exposeConfig: false,
-    viewer: true,
-  },
 
-  compatibilityDate: '2024-12-21',
+    routeRules: {
+        // SPA geçişleri için
+        '/': {prerender: true},
+        '/products/**': {swr: 3600},
+        '/categories/**': {swr: 3600},
+    },
+
+    tailwindcss: {
+        cssPath: '~/assets/css/main.css',
+        configPath: 'tailwind.config.js',
+        exposeConfig: false,
+        viewer: true,
+    },
+
+    compatibilityDate: '2024-12-21',
 })
