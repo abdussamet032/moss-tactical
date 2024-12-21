7e976ef8bcd5b76b2f26c324c2e266939abda03c
abdussametalisar <abdussamet032@gmail.com>
2024-12-22 00:25:18 +0300

    fix deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,7 +53,7 @@ export default defineNuxtConfig({
     supabase: {
         url: process.env.SUPABASE_URL,
         key: process.env.SUPABASE_KEY,
-        redirectOptions: {
+        redirectOptions: {*
             login: '/auth/login',
             callback: '/confirm',
             exclude: ['/*'],
