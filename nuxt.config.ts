7976431845d95f286580879f8782d8e25b189bc9
abdussametalisar <abdussamet032@gmail.com>
2025-06-27 17:41:04 +0300

    bismillah 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     // Server-side (private) environment variables
-    openaiApiKey: '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',
+    openaiApiKey: process.env.OPENAI_API_KEY,
     public: {
       supabaseUrl: 'https://tffakpklfsvcmoskekgt.supabase.co',
       supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmZmFrcGtsZnN2Y21vc2tla2d0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNDQ4NTM0MywiZXhwIjoyMDUwMDYxMzQzfQ.u-ijQoG52FpW8_KU8K145Qsq0EdI7Tw16L5d1jTKEZM',
